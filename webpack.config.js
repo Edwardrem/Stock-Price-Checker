f59296420748e3f542f192a88d16978b62b99622
Glitch (starter-react) <none>
2019-06-25 17:43:36 +0000

    🦁🏀 Checkpoint ./app/app.jsx:3405442/3071 ./webpack.config.js:3405442/7

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   mode: 'production',
   context: path.join(__dirname, './'),
-  entry: './app/app.jsx',
+  entry: './app/main.jsx',
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js',
