4c5b96da57927d9854caf8f40cf6d4f402689bd8
Glitch (starter-react) <none>
2019-06-25 22:35:51 +0000

    🔔🍖 Checkpoint ./app/app.jsx:3405442/331

--- app/app.jsx
+++ app/app.jsx
@@ -5,16 +5,20 @@ const App = () => {
   
   const [date, setDate] = useState('');
   
+  const handleSubmit = () => {
+    window.location.href = "https://fcc-bt-timestamp-microservice.glitch.me/api/timestamp/" + date;
+  }
+  
   return (
     <main>
     <div className="container">
         <h2>API Project: Timestamp Microservice</h2>
         <a href="https://fcc-bt-timestamp-microservice.glitch.me/api/timestamp/">Get timestamp for today</a><br/>
         
-        <form>
+        <form onSubmit={handleSubmit}>
           Get timestamp:<br/>
-          <input type="text" placeholder="2016-11-20" name="date" onChange={e => setDate(e.target.value}/>
-          <a href={"https://fcc-bt-timestamp-microservice.glitch.me/api/timestamp/" + date}>Get timestamp</a>
+          <input type="text" placeholder="2016-11-20" name="date" onChange={e => setDate(e.target.value)}  />
+          <input type="submit" value="Submit"/>
         </form>
         
         
