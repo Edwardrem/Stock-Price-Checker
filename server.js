41d536249b5b4cb6448571f71ec42a91ef44ee21
Glitch (starter-react) <none>
2019-06-27 16:31:08 +0000

    🌝🚆 Checkpoint ./server.js:3405442/4796 ./test-runner.js:3405442/2140 ./assertion-analyser.js:3405442/2985

--- server.js
+++ server.js
@@ -6,30 +6,53 @@ const expect      = require('chai').expect;
 const cors        = require('cors');
 const helmet      = require('helmet'); 
 
-const app = express()
+const apiRoutes         = require('./routes/api.js');
+const fccTestingRoutes  = require('./routes/fcctesting.js');
+const runner            = require('./test-runner');
 
-app.use(cors())
+const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(helmet());
 
 app.use(express.static('public'))
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function(request, response) {
-  response.sendFile(__dirname + '/view/index.html');
+app.use(cors({origin: '*'})); //For FCC testing purposes only
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Index page (static HTML)
+app.route('/')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+//For FCC testing purposes
+fccTestingRoutes(app);
+
+//Routing for API 
+apiRoutes(app);  
+    
+//404 Not Found Middleware
+app.use(function(req, res, next) {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
 });
 
-
-
-// Not found middleware
-app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
-})
-
-
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+//Start our server and tests!
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Listening on port " + process.env.PORT);
+  if(process.env.NODE_ENV==='test') {
+    console.log('Running Tests...');
+    setTimeout(function () {
+      try {
+        runner.run();
+      } catch(err) {
+          console.log('Tests are not valid:', err);
+      }
+    }, 1500);
+  }
 });
+
+module.exports = app; //for testing
