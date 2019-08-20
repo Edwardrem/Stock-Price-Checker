be24057ac6216ed75fc745255d1610c1540c4f6e
Glitch (starter-react) <none>
2019-08-20 17:37:21 +0000

    🚍⛷ Checkpoint ./package.json:3405442/331 ./webpack.config.js:3405442/1430

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,37 @@
-const path = require('path');
+var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
-  mode: 'production',
-  context: path.join(__dirname, './'),
-  entry: './views/main.jsx',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js',
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        loader: 'jsx-loader',
-        exclude: /node_modules/,
-        include: path.join(__dirname, 'views'),
-      },
-    ],
-  },
+    mode: 'development',
+    entry: ['./views/main.jsx'],
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {   
+                test: /\.jsx?$/,
+                include: [
+                    path.resolve(__dirname, 'views'),
+                ],
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                        plugins: [
+                          '@babel/plugin-proposal-class-properties',
+                        ]
+                    }
+                }
+            },
+            {
+              test: /\.s?css$/,
+              use: ["style-loader", "css-loader", "sass-loader"]
+            },
+        ]
+    },
+    node: {
+      fs: "empty"
+    }
 };
