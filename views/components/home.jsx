511020297631ec298418397f69c63e1dd7cc8041
Glitch (starter-react) <none>
2019-08-21 18:10:32 +0000

    🚆👽 Checkpoint ./views/components/home.jsx:3405442/43 ./views/components/temp.jsx:3405442/3508

--- views/components/home.jsx
+++ views/components/home.jsx
@@ -5,13 +5,12 @@ import AppTitle from './common/appTitle.jsx';
 
 const Home = () => {
   
-  const [books, setBooks] = useState([]);
   
   return (
     <div className="inner-container">
       
       <AppTitle title={<h1>Stock Price Checker</h1>} />
-     
+      
       
     </div>
   );
