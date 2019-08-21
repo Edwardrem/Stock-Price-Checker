c188e40421a6d10094711213ab5f90b26c36bc50
Glitch (starter-react) <none>
2019-08-21 18:21:31 +0000

    💅💈 Checkpoint ./views/components/stockChecker.jsx:3405442/3906

--- views/components/stockChecker.jsx
+++ views/components/stockChecker.jsx
@@ -33,48 +33,41 @@ const App = () => {
   }
   
   return (
-    <main className='container'>
-      <div id='userstories'>
-        <h3>User Stories</h3>
-        <ol>
-          <li>Set the content security policies to only allow loading of scripts and css from your server.</li>
-          <li>I can <b>GET</b> <code>/api/stock-prices</code> with form data containing a Nasdaq <i>stock</i> ticker and recieve back an object <i>stockData</i>.</li>
-          <li>In <i>stockData</i>, I can see the <i>stock</i>(string, the ticker), <i>price</i>(decimal in string format), and <i>likes</i>(int).</li>
-          <li>I can also pass along field <i>like</i> as <b>true</b>(boolean) to have my like added to the stock(s). Only 1 like per ip should be accepted.</li>
-          <li>If I pass along 2 stocks, the return object will be an array with both stock's info but instead of <i>likes</i>, it will display <i>rel_likes</i>(the difference between the likes on both) on both.</li>
-          <li>A good way to receive current price is the following external API(replacing 'GOOG' with your stock): <code>https://finance.google.com/finance/info?q=NASDAQ%3aGOOG</code></li>
-          <li>All 5 functional tests are complete and passing.</li>
-        </ol>
-        <h3>Example usage:</h3>
-        <code>/api/stock-prices?stock=goog</code><br/>
-        <code>/api/stock-prices?stock=goog&amp;like=true</code><br/>
-        <code>/api/stock-prices?stock=goog&amp;stock=msft</code><br/>
-        <code>/api/stock-prices?stock=goog&amp;stock=msft&amp;like=true</code><br/>
-        <h3>Example return:</h3>
-        <code>{'{'}"stockData":{'{'}"stock":"GOOG","price":"786.90","likes":1{'}}'}</code><br/>
-        <code>{'{'}"stockData":[{'{'}"stock":"MSFT","price":"62.30","rel_likes":-1{'}'},{'{'}"stock":"GOOG","price":"786.90","rel_likes":1{'}]}'}</code>
+   <React.Fragment>
+      
+      <div className="submit-book">
+        <h3>Get single price and total likes</h3>
+
+        <div className="form-container">
+
+            <form className="submit-book-form" onSubmit={handleSubmit}>
+
+              <input className="form-title" type="text" name="title" placeholder="*Book Title" required/>
+              <input className="form-submit" type="submit" value="Add Book"/>
+
+            </form>
+
+        </div>
+
       </div>
       
-      <hr />
-    <div id='testui'>
-      <h2 >Front-End:</h2>
-      <h3>Get single price and total likes</h3>
-      <form id="testForm2" class="border" onSubmit={handleSubmit}>
-        <input type="text" name="stock" placeholder="goog"  required/>
-        <input type="checkbox" name="like" value={true}/> Like?<br/>
-        <input type="submit" value="Get Price!"/>
-      </form>
+      <div className="submit-book">
       <h3>Compare and get relative likes</h3>
-      <form id="testForm" class="border" onSubmit={handleSubmit}>
-        <input type="text" name="stock" placeholder="goog"  required/>
-        <input type="text" name="stock" placeholder="msft"  required/>
-        <input type="checkbox" name="like" value={true}/> Like both?<br/>
-        <input type="submit" value="Get Price!"/>
-      </form>
-      <code id='jsonResult'></code>
+      
+      <div className="form-container">
+        
+          <form className="submit-book-form" onSubmit={handleSubmit}>
+            
+            <input className="form-title" type="text" name="title" placeholder="*Book Title" required/>
+            <input className="form-submit" type="submit" value="Add Book"/>
+            
+          </form>
+        
+      </div>
+      
     </div>
-    <hr />
-    </main>
+      
+   </React.Fragment>
   )
 }
 
